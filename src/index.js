4006338e8ddeb17f3f58fef9af63afd93b07f6dc
Kudesniza <kudesniza@yandex.ru>
2020-09-30 00:06:11 +0300

    init: typical array problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  return array.sort((a,b)=>a-b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0 ) {
+    return 0;
+  }
+  return array.sort((a,b)=>b-a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0 ) {
+    return 0;
+  }
+  let result = 0;
+  for (let i = 0; i <array.length; i++) {
+    result += array[i];
+  }
+  return result/array.length;
 }
